e941d8f965bbc57cc27d4f7b4b9a9563a34c7074
planttheidea <tony.quetano@planttheidea.com>
2014-12-04 16:51:35 -0500

    Update bolster.js

--- bolster.js
+++ bolster.js
@@ -967,7 +967,7 @@
 			}
 			
 			// performs subscription (abstrated for the same reason as above unsubscription)
-			function prv_subscribeTopic(topic,newToken,fn,once,name,newToken){
+			function prv_subscribeTopic(topic,fn,once,name,newToken){
 				if($.type(topics[topic]) !== 'array'){
 					topics[topic] = [];
 				}
@@ -1007,12 +1007,12 @@
 				// subscriptions called differently depending on typ
 				switch($.type(subscribeObj.topic)){
 					case 'string':
-						prv_subscribeTopic(subscribeObj.topic,subscribeObj.token,subscribeObj.fn,subscribeObj.once,subscribeObj.name,IDs[subscribeObj.name]);
+						prv_subscribeTopic(subscribeObj.topic,subscribeObj.fn,subscribeObj.once,subscribeObj.name,IDs[subscribeObj.name]);
 
 						break;
 					case 'array':					
 						for(var i = subscribeObj.topic.length; i--;){
-							prv_subscribeTopic(subscribeObj.topic[i],subscribeObj.token,subscribeObj.fn,subscribeObj.once,subscribeObj.name,IDs[subscribeObj.name]);
+							prv_subscribeTopic(subscribeObj.topic[i],subscribeObj.fn,subscribeObj.once,subscribeObj.name,IDs[subscribeObj.name]);
 						};
 						
 						break;
